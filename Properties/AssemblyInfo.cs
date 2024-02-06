af10d37c6585f14987f460e405a496192edda8ae
Brennan Davis <62734507+brennan-davis@users.noreply.github.com>
2024-02-06 16:40:42 -0500

    Initial Commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b078f110-a9b2-4887-9912-20979fae2b03")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class SeleniumTest
+    {
+        public string test_url = "https://aptitude8.com/";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver;
+
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl(test_url);
+
+            driver.Manage().Window.Maximize();
+
+            System.Threading.Thread.Sleep(2000);
+
+            IWebElement contactUsButton = driver.FindElement(By.XPath("//*[@id=\"hs_cos_wrapper_module_16106654906244\"]/div/div/div/div/div/a/button"));
+            contactUsButton.Click();
+
+            IWebElement firstNameInput = driver.FindElement(By.XPath("//*[@id=\"firstname-21744f0f-c39d-4622-ad8c-9aac3b93d0a3_8281\"]"));
+            firstNameInput.SendKeys("Brennan");
+
+            IWebElement lastNameInput = driver.FindElement(By.XPath("//*[@id=\"lastname-21744f0f-c39d-4622-ad8c-9aac3b93d0a3_8281\"]"));
+            lastNameInput.SendKeys("Davis");
+
+
+            IWebElement header = driver.FindElement(By.XPath("//*[@id=\"hs_cos_wrapper_section-1-module-2_\"]/p/span/span"));
+            string headerText = header.Text;
+            Assert.IsTrue(headerText.ToLower().Contains("contact"));
+
+            System.Threading.Thread.Sleep(4000);
+
+            // Success
+            Console.WriteLine("Contact Us Page Reached!");
+            driver.Quit();
+        }
+    }
+}
